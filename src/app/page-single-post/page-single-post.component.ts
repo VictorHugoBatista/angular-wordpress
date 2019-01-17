9d8c455375443d46daf91b14f3d874febe86df27
Victor Hugo <vct.bta@gmail.com>
2019-01-17 19:25:55 -0200

    Take postSlug with param mapping on single post component and route

--- src/app/page-single-post/page-single-post.component.ts
+++ src/app/page-single-post/page-single-post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-page-single-post',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-single-post.component.scss']
 })
 export class PageSinglePostComponent implements OnInit {
+  private postSlug : string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.postSlug = this.route.snapshot.paramMap.get('slug');
+    // take post on API and add to attribute used on component html
   }
 
 }
