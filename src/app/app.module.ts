ea7341a573a68ccdb21ac2ee13adf6c4a5de4e21
Victor Hugo <vct.bta@gmail.com>
2019-01-16 23:54:18 -0200

    Add new components for pages listing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListPostsComponent } from './list-posts/list-posts.component';
 import { PagePostsComponent } from './page-posts/page-posts.component';
+import { PagePagesComponent } from './page-pages/page-pages.component';
+import { ListPagesComponent } from './list-pages/list-pages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListPostsComponent,
-    PagePostsComponent
+    PagePostsComponent,
+    PagePagesComponent,
+    ListPagesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/list-pages/list-pages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-pages',
+  templateUrl: './list-pages.component.html',
+  styleUrls: ['./list-pages.component.scss']
+})
+export class ListPagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
