a14f2649bad08da60548a0338a4999ea6f5539ca
Victor Hugo <vct.bta@gmail.com>
2019-01-17 18:59:25 -0200

    Dinamize per_page parameter on list-posts component

--- src/app/list-posts/list-posts.component.ts
+++ src/app/list-posts/list-posts.component.ts
@@ -9,13 +9,14 @@ import { Observable } from 'rxjs';
 })
 export class ListPostsComponent implements OnInit {
   @Input() postType : string;
+  @Input() perPage : Number;
   public posts : Observable<any[]>;
 
   constructor(private service : ListPostsService) {}
 
   ngOnInit() {
     this.posts = this.service.getPosts(this.postType, {
-      per_page: 6
+      per_page: this.perPage,
     });
   }
 
