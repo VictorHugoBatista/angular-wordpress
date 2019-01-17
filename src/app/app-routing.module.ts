be5708826685fb28332bd37e802e3d88d70d4e7b
Victor Hugo <vct.bta@gmail.com>
2019-01-17 19:16:26 -0200

    Add posts/:slug route for single page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PagePostsComponent } from './page-posts/page-posts.component';
 import { PagePagesComponent } from './page-pages/page-pages.component';
+import { PageSinglePostComponent } from './page-single-post/page-single-post.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'pages',
     component: PagePagesComponent,
+  },
+  {
+    path: 'posts/:slug',
+    component: PageSinglePostComponent,
   },
 ];
 
