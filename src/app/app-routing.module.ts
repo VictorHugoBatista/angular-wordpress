d14cb91a638df8d0abf49e448b46ba50cf76ffb2
Victor Hugo <vct.bta@gmail.com>
2019-01-16 22:59:26 -0200

    Add route /posts to PagePostsComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagePostsComponent } from './page-posts/page-posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'posts',
+    component: PagePostsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
