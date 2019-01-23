152a9c9f4f02cc1e5d3e83d9ee1374749b1c2d1d
Victor Hugo <vct.bta@gmail.com>
2019-01-23 08:32:45 -0200

    Add the single page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { PagePostsComponent } from './page-posts/page-posts.component';
 import { PagePagesComponent } from './page-pages/page-pages.component';
 import { PageSinglePostComponent } from './page-single-post/page-single-post.component';
+import { PageSinglePageComponent } from './page-single-page/page-single-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
@@ -17,6 +18,10 @@ const routes: Routes = [
   {
     path: 'posts/:slug',
     component: PageSinglePostComponent,
+  },
+  {
+    path: 'pages/:slug',
+    component: PageSinglePageComponent,
   },
   {
     path: '**',
