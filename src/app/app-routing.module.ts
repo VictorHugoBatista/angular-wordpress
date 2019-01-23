af262d2772300dbe3146c77c9c1a9ccaf86a9017
Victor Hugo <vct.bta@gmail.com>
2019-01-22 23:34:16 -0200

    Add route to page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { PagePostsComponent } from './page-posts/page-posts.component';
 import { PagePagesComponent } from './page-pages/page-pages.component';
 import { PageSinglePostComponent } from './page-single-post/page-single-post.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'posts/:slug',
     component: PageSinglePostComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
   },
 ];
 
