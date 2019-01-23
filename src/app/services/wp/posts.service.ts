74acbdf597dfc728e8e9671fcc657dce55d13bc4
Victor Hugo <vct.bta@gmail.com>
2019-01-22 22:35:29 -0200

    Change getPosts method to new service class

--- src/app/services/wp/posts.service.ts
+++ src/app/services/wp/posts.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getPosts(postType : string, params) : Observable<any[]> {
+    return this.http.get<any[]>(`https://www.oxygenna.com/wp-json/wp/v2/${postType}`, {
+      params,
+    });
+  }
 }
