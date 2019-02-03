8856e456a03c527925545ac17b6ff34eb3fa3397
Victor Hugo <vct.bta@gmail.com>
2019-02-03 18:00:00 -0200

    Document pagination feature.

--- src/app/components/list-posts/list-posts.component.ts
+++ src/app/components/list-posts/list-posts.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit, Input } from '@angular/core';
 import { PostsService } from '../../services/wp/posts.service';
 import { Subject } from 'rxjs';
 
+/**
+ * Componente de listagem de um post type específico na API
+ * do WordPress. Chama o componente PaginationComponent no HTML.
+ *
+ * @Input() postType : Slug do post type à ser buscado na API do WordPress.
+ * @Input() perPage : Número de posts exibidos por página.
+ */
 @Component({
   selector: 'app-list-posts',
   templateUrl: './list-posts.component.html',
@@ -10,19 +17,45 @@ import { Subject } from 'rxjs';
 export class ListPostsComponent implements OnInit {
   @Input() public postType : string;
   @Input() public perPage : Number;
+
+  /**
+   * Posts exibidos na listagem, recebidos da API.
+   */
   public posts : Array<any>;
+
+  /**
+   * Evento de fim do carregamento dos posts da API.
+   */
   private endLoadingEvent : Subject<Number> = new Subject<Number>();
 
+  /**
+   * Recebe o service que faz a interface com a API do WordPress.
+   * @param service {PostsService}.
+   */
   constructor(private service : PostsService) {}
 
+  /**
+   * Carrega a primeira página de posts da API do WordPress
+   * no carregamento da página do front Angular.
+   */
   ngOnInit() {
     this.getPostsPage();
   }
 
+  /**
+   * Recarrega a listagem com os posts de uma nova página.
+   * @param newPageNumber 
+   */
   public changePage(newPageNumber) {
     this.getPostsPage(newPageNumber);
   }
 
+  /**
+   * Carrega os posts de uma página na API do WordPress e
+   * carrega no template. Também atualiza o componente
+   * de paginação com os novos dados.
+   * @param pageNumber {Number} : Número da página. Padrão = 1.
+   */
   private getPostsPage(pageNumber : Number = 1) {
     this.service.getPosts(this.postType, {
       per_page: this.perPage,
--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Observable } from 'rxjs';
 
+/**
+ * Gera a paginação do bootstrap na tela.
+ *
+ * @Input startTotalPages : observable que faz trigger com
+ * o número de páginas para a geração do componente.
+ * @Input shownPageSteps : número de itens exibidos antes
+ * e depois do item da página atual.
+ * @Output pageSelected : Evento de página selecionada.
+ * Passa o número da página atual para a busca em uma API,
+ * por exemplo.
+ */
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
@@ -9,16 +20,35 @@ import { Observable } from 'rxjs';
 export class PaginationComponent implements OnInit {
   @Input() public startTotalPages : Observable<any>;
   @Input() public shownPageSteps : any;
+  @Output() public pageSelected = new EventEmitter();
 
+  /**
+   * Página atual exibida no front. Utilizada nos cálculos
+   * de exibição do componente na página.
+   */
   public currentPage : any = 1;
+
+  /**
+   * Número total de páginas exibidas. Utilizada nos cálculos
+   * de exibição do componente na página.
+   */
   public totalPages : Number;
+
+  /**
+   * Itens exibidos na páginação. Array de objetos com
+   * o número de página e uma flag que indica se
+   * o elemento possui a classe active.
+   */
   public paginationItems : Array<Object> = [];
 
   public startTotalPagesSubscription : any;
-  @Output() public pageSelected = new EventEmitter();
 
   constructor() { }
 
+  /**
+   * Atualiza o componente na tela à cada vez que o evento
+   * passado no @Input startTotalPages for executado.
+   */
   ngOnInit() {
     this.startTotalPagesSubscription =
       this.startTotalPages.subscribe(totalPages => {
@@ -27,38 +57,58 @@ export class PaginationComponent implements OnInit {
       });
   }
 
-  public onNumberClick(newPageNumber) {
+  /**
+   * Evento de click dos elementos do componente.
+   *
+   * @param newPageNumber {Number} : Número da nova página atual.
+   */
+  public onNumberClick(newPageNumber : Number) {
     this.currentPage = newPageNumber;
     this.pageSelected.next(newPageNumber);
   }
 
+  /**
+   * Gera o elemento de paginação na tela.
+   */
   private regeneratePagination() {
     let pages = [];
-    // Previous pages.
+    // Exibe as páginas anteriores limitadas pelo @Input() shownPageSteps
+    // até a segunda página.
     for (let i = this.currentPage - 1;
       i >= this.currentPage - this.shownPageSteps && i > 1; i--) {
       pages.push(this.generatePageItem(i));
     }
-    // First page.
+    // Exibe a primeira página separadamente, a página atual não seja ela mesma.
     if (1 < this.currentPage) {
       pages.push(this.generatePageItem(1));
     }
+    // As páginas anteriores são geradas na ordem crescente.
+    // Deve-se reverter para exibir o componente corretamente.
     pages.reverse();
-    // Current page, marked.
+    // Página atual.
     pages.push(this.generatePageItem(this.currentPage, true));
-    // Next pages.
+    // Exibe as próximas páginas limitadas pelo @Input() shownPageSteps
+    // até a penúltima página, recebida no evento passado no
+    // @Input startTotalPages.
     for (let i = this.currentPage + 1;  
       i <= this.currentPage + this.shownPageSteps && i < this.totalPages; i++) {
       pages.push(this.generatePageItem(i));
     }
-    // Last page.
+    // Exibe a última página separadamente, a página atual não seja ela mesma.
     if (this.currentPage < this.totalPages) {
       pages.push(this.generatePageItem(this.totalPages));
     }
+    // Adiciona o array gerado ao array que gera o html no template.
     this.paginationItems = pages;
   }
 
-  private generatePageItem(pageNumber, hasActiveClass = false) {
+  /**
+   * Gera um objeto de página adicionado ao array lido pelo componente no template.
+   *
+   * @param pageNumber {Number} : Número da página à ser gerada.
+   * @param hasActiveClass {Boolean} : Indica se o elemento possui a classe 'active'. Padrão = false.
+   */
+  private generatePageItem(pageNumber : Number, hasActiveClass : Boolean = false) {
     return {
       page_number: pageNumber,
       active: hasActiveClass,
