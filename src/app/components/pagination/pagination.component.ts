69096615fba238aaeb1bf2aabe1d976c64418f39
Victor Hugo <vct.bta@gmail.com>
2019-02-03 11:09:36 -0200

    Split the pagination regeneration to another method

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -9,21 +9,25 @@ import { Observable } from 'rxjs';
 export class PaginationComponent implements OnInit {
   @Input() public currentPage : Number;
   @Input() public startTotalPages : Observable<any>;
-  public startTotalPagesSubscription : any;
   public totalPages : Number;
   public paginationItems : Array<Object> = [];
+  public startTotalPagesSubscription : any;
 
   constructor() { }
 
   ngOnInit() {
     this.startTotalPagesSubscription = this.startTotalPages.subscribe(totalPages => {
       this.totalPages = totalPages;
-      this.paginationItems = Array(this.totalPages).fill(0).map((_, item) => {
-        return {
-          page_number: item + 1,
-          active: item + 1 === this.currentPage,
-        } ;
-      });
+      this.regeneratePagination();
+    });
+  }
+
+  private regeneratePagination() {
+    this.paginationItems = Array(this.totalPages).fill(0).map((_, item) => {
+      return {
+        page_number: item + 1,
+        active: item + 1 === this.currentPage,
+      } ;
     });
   }
 }
